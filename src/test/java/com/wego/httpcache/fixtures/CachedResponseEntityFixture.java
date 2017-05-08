e50a9422e6de0e116a0bd92f17fdb984f1750c54
Tam Nguyen <tamnguyenit@users.noreply.github.com>
2017-05-08 20:02:27 +0800

    add missing fixture

---
+++ src/test/java/com/wego/httpcache/fixtures/CachedResponseEntityFixture.java
@@ -0,0 +1,17 @@
+package com.wego.httpcache.fixtures;
+
+import com.wego.httpcache.dao.entities.CachedResponseEntity;
+import java.util.Arrays;
+
+public class CachedResponseEntityFixture {
+  public static CachedResponseEntity create() {
+    return new CachedResponseEntity.Builder()
+        .setId("ID")
+        .setResponseBody("Body")
+        .setStatusCode(200)
+        .setStatusText("")
+        .setHeaders(HeaderFixture.create())
+        .setCookies(Arrays.asList(CookieFixture.create()))
+        .build();
+  }
+}
---
+++ src/test/java/com/wego/httpcache/fixtures/CookieFixture.java
@@ -0,0 +1,14 @@
+package com.wego.httpcache.fixtures;
+
+import com.ning.http.client.cookie.Cookie;
+import org.apache.commons.lang3.StringUtils;
+
+public class CookieFixture {
+  public static Cookie create() {
+    return createWithKeyAndValue("key", "value");
+  }
+
+  public static Cookie createWithKeyAndValue(String key, String value) {
+    return new Cookie(key, value, StringUtils.join(key, "=", value), "", "", 1, 2, true, true);
+  }
+}
