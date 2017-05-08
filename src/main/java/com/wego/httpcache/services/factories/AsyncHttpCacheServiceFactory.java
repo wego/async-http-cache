fe15c0d68d2e1129ca755774d037292549415d87
Tam Nguyen <tamnguyenit@users.noreply.github.com>
2017-05-08 17:41:23 +0800

    rename AsyncHttpCacheServiceFactory

--- src/main/java/com/wego/httpcache/services/factories/AsyncHttpCacheServiceFactory.java
+++ src/main/java/com/wego/httpcache/services/factories/AsyncHttpCacheServiceFactory.java
@@ -3,6 +3,6 @@ package com.wego.httpcache.services.factories;
 import com.ning.http.client.AsyncHttpClient;
 import com.wego.httpcache.services.AsyncHttpCacheService;
 
-public interface AsyncHttpServiceFactory {
+public interface AsyncHttpCacheServiceFactory {
   AsyncHttpCacheService create(AsyncHttpClient asyncHttpClient, long ttl);
 }
