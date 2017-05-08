69fdf9025fd888a739a997fc0abff5c880ba5d6d
Tam Nguyen <tamnguyenit@users.noreply.github.com>
2017-05-08 14:14:06 +0800

    add TestCachedResponseServiceImpl

---
+++ src/test/java/com/wego/services/impl/TestCachedResponseServiceImpl.java
@@ -0,0 +1,78 @@
+package com.wego.services.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Lists;
+import com.wego.dao.CachedResponseDao;
+import com.wego.dao.entities.CachedResponseEntity;
+import com.wego.dao.mappers.CachedResponseEntityToCachedResponse;
+import com.wego.dao.mappers.CachedResponseToCachedResponseEntity;
+import com.wego.dao.models.CachedResponse;
+import com.wego.services.CachedResponseService;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestCachedResponseServiceImpl {
+  @InjectMocks
+  private CachedResponseService cachedResponseService = new CachedResponseServiceImpl();
+
+  @Mock private CachedResponseDao cachedResponseDao;
+  @Mock private CachedResponseEntityToCachedResponse cachedResponseEntityToCachedResponse;
+  @Mock private CachedResponseToCachedResponseEntity cachedResponseToCachedResponseEntity;
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @Test
+  public void save_whenInputObjectIsNull_returnNull() {
+    assertThat(cachedResponseService.save(null)).isNull();
+  }
+
+  @Test
+  public void save_whenInputObjectIsNotNull_saveCachedResponse() throws Exception {
+    final CachedResponse cachedResponse = new CachedResponse();
+    final CachedResponseEntity cachedResponseEntity = new CachedResponseEntity();
+    final List<CachedResponse> savedCachedResponses = Lists.newArrayList();
+
+    when(cachedResponseToCachedResponseEntity.transform(cachedResponse))
+        .thenReturn(cachedResponseEntity);
+    when(cachedResponseDao.save(any(CachedResponseEntity.class)))
+        .thenAnswer(
+            invocation -> {
+              savedCachedResponses.add(invocation.getArgumentAt(0, CachedResponse.class));
+              return cachedResponseEntity;
+            });
+
+    assertThat(cachedResponseService.save(cachedResponse)).isEqualTo(cachedResponse);
+    assertThat(savedCachedResponses.size()).isEqualTo(1);
+  }
+
+  @Test
+  public void findById_whenNotFound_returnNull() throws Exception {
+    when(cachedResponseDao.findById(anyString())).thenReturn(Optional.empty());
+
+    assertThat(cachedResponseService.findById("ID").isPresent()).isFalse();
+  }
+
+  @Test
+  public void findById_whenFound_returnOptionalOfCachedResponse() throws Exception {
+    final CachedResponse cachedResponse = new CachedResponse();
+    final CachedResponseEntity cachedResponseEntity = new CachedResponseEntity();
+
+    when(cachedResponseEntityToCachedResponse.transform(cachedResponseEntity))
+        .thenReturn(cachedResponse);
+    when(cachedResponseDao.findById("ID")).thenReturn(Optional.of(cachedResponseEntity));
+
+    assertThat(cachedResponseService.findById("ID").get()).isEqualTo(cachedResponse);
+  }
+}
