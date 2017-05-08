ed96f351142000eb60fc95979abc48cf5b271366
Tam Nguyen <tamnguyenit@users.noreply.github.com>
2017-05-08 20:02:16 +0800

    cache exception and log

--- src/main/java/com/wego/httpcache/services/impl/CachedResponseServiceImpl.java
+++ src/main/java/com/wego/httpcache/services/impl/CachedResponseServiceImpl.java
@@ -6,9 +6,13 @@ import com.wego.httpcache.dao.mappers.CachedResponseEntityToCachedResponse;
 import com.wego.httpcache.dao.mappers.CachedResponseToCachedResponseEntity;
 import com.wego.httpcache.dao.models.CachedResponse;
 import com.wego.httpcache.services.CachedResponseService;
+import java.io.IOException;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CachedResponseServiceImpl implements CachedResponseService {
+  private static final Logger LOGGER = LoggerFactory.getLogger(CachedResponseServiceImpl.class);
 
   @Inject private CachedResponseDao cachedResponseDao;
 
@@ -22,7 +26,11 @@ public class CachedResponseServiceImpl implements CachedResponseService {
       return null;
     }
 
-    cachedResponseDao.save(responseToHttpReponseEntity.transform(cachedResponse), ttl);
+    try {
+      cachedResponseDao.save(responseToHttpReponseEntity.transform(cachedResponse), ttl);
+    } catch (IOException e) {
+      LOGGER.error("Error saving cachedResponse with id {}", cachedResponse.getId(), e);
+    }
 
     return cachedResponse;
   }
